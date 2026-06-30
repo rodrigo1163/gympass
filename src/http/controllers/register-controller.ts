027f7807780c74973fc40d03b03b7bfbb215d251
rodrigo.sena <rodrigo.sena@enablers.com.br>
2026-06-29 22:17:28 -0400

    feat: add bcryptjs for password hashing and enhance user registration logic

--- src/http/controllers/register-controller.ts
+++ src/http/controllers/register-controller.ts
@@ -1,22 +1,35 @@
 import { FastifyRequest, FastifyReply } from "fastify"
 import { prisma } from "@/lib/prisma"
 import z from "zod"
+import { hash } from 'bcryptjs'
 
 export async function registerController(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
-    email: z.string().email(),
+    email: z.email(),
     password: z.string().min(6),
   })
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
+  const password_hash = await hash(password, 8)
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
-    },
+      password_hash,
+    }
   })
 
   return reply.status(201).send()
