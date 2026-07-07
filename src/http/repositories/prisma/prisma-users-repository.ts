5c223106d6d1224558b1adf455665e36dff0a866
rodrigo.sena <rodrigo.sena@enablers.com.br>
2026-07-07 10:36:25 -0400

    feat: add findById method to PrismaUsersRepository

--- src/http/repositories/prisma/prisma-users-repository.ts
+++ src/http/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,12 @@
+import type { User } from '../../../../prisma/generated/prisma/browser'
 import type { UserCreateInput } from '../../../../prisma/generated/prisma/models'
 import { prisma } from '../../../lib/prisma'
 import type { UsersRepository } from '../users-repository'
 
 export class PrismaUsersReposity implements UsersRepository {
+  findById(userId: string): Promise<User | null> {
+    throw new Error('Method not implemented.')
+  }
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
