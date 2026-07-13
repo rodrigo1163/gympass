de768187c0565ca51639d67ca9104d6aac027c0a
rodrigo.sena <rodrigo.sena@enablers.com.br>
2026-07-13 10:44:00 -0400

    feat: add gyms routes for creating and searching gyms, including nearby gyms functionality

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { ZodError, z } from 'zod'
 import { env } from './env'
 import { usersRoutes } from './http/controllers/users/routes'
 import fastifyJwt from '@fastify/jwt'
+import { gymsRoutes } from './http/controllers/gyms/routes'
 
 export const app = fastify()
 
@@ -11,6 +12,7 @@ app.register(fastifyJwt, {
 })
 
 app.register(usersRoutes)
+app.register(gymsRoutes)
 
 app.setErrorHandler((error, _, reply) => {
   if (error instanceof ZodError) {
--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import type { FastifyInstance } from 'fastify'
+import { createGymBodyController } from './create-controller'
+import { searchGymsController } from './search-controller'
+import { nearbyGymsController } from './nearby-controller'
 
 export async function gymsRoutes(app: FastifyInstance) {
     app.addHook('onRequest', verifyJWT)
+
+    app.post('/gyms', createGymBodyController)
+    
+    app.get('/gyms/search', searchGymsController)
+    app.get('/gyms/nearby', nearbyGymsController)
 }
